bb078332f266130d83c7200aa14d8f74f09f4da4
Bruno Sanchez <sanchezbruno689@gmail.com>
2026-02-13 15:12:30 -0300

    Refactor Macbook model components to improve texture handling and code consistency

--- src/components/models/Macbook-16.jsx
+++ src/components/models/Macbook-16.jsx
@@ -18,11 +18,14 @@ export default function Macbook16(props) {
   const {color} = useMacbookStore()
   const { nodes, materials, scene } = useGLTF('/models/macbook-16-transformed.glb')
   const texture = useTexture('/screen.png')
+  texture.colorSpace = THREE.SRGBColorSpace;
+  texture.needsUpdate = true;
+
   useEffect(() => {
-    scene.traverse(child => {
+    scene.traverse((child) => {
       if (child.isMesh) {
-        if (!noChangeParts.includes(child.name)){
-          child.material.color = new THREE.Color(color)
+        if (!noChangeParts.includes(child.name)) {
+          child.material.color = new THREE.Color(color);
         }
       }
     })
@@ -46,7 +49,7 @@ export default function Macbook16(props) {
       <mesh geometry={nodes.Object_82.geometry} material={materials.gMtYExgrEUqPfln} rotation={[Math.PI / 2, 0, 0]} />
       <mesh geometry={nodes.Object_96.geometry} material={materials.PaletteMaterial003} rotation={[Math.PI / 2, 0, 0]} />
       <mesh geometry={nodes.Object_107.geometry} material={materials.JvMFZolVCdpPqjj} rotation={[Math.PI / 2, 0, 0]} />
-      <mesh geometry={nodes.Object_123.geometry} material={materials.sfCQkHOWyrsLmor} rotation={[Math.PI / 2, 0, 0]}>
+      <mesh geometry={nodes.Object_123.geometry} rotation={[Math.PI / 2, 0, 0]}>
         <meshBasicMaterial map={texture} />
       </mesh>
       <mesh geometry={nodes.Object_127.geometry} material={materials.ZCDwChwkbBfITSW} rotation={[Math.PI / 2, 0, 0]} />
